18f8a377d4a8cd0ecadf51805fa8e105d538ad32
CodeSmashing <1notmymail1@gmail.com>
2025-03-21 14:30:04 +0100

    update(module-8): Update easy Module 8 assignment
    - e1_js.js:
        • Change variable themeToggle to themeToggleButton to be lightly more descriptive
        • Change varaible currentTheme to themePreference as it made more sense
        • Use .className instead of .classList as it fits better
        • Update toggleTheme() function to now change the button text content depending on what theme was picked
        • Add loadTheme() function meant to initially set the theme based on localStorage

--- Module 8/Easy/e1_js.js
+++ Module 8/Easy/e1_js.js
@@ -1,15 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const themeToggle = document.querySelector("#themeToggle");
-	themeToggle.addEventListener("click", toggleTheme);
+	const themeToggleButton = document.querySelector("#themeToggle");
+	themeToggleButton.addEventListener("click", toggleTheme);
 
 	function toggleTheme() {
-		const currentTheme = localStorage.getItem("preference") || "dark";
+		const themePreference = localStorage.getItem("preference") || "dark";
 		const themeOpposites = {
 			dark: "light",
 			light: "dark",
 		};
 
-		document.body.classList.replace(`${currentTheme}-theme`, `${themeOpposites[currentTheme]}-theme`);
-		localStorage.setItem("preference", themeOpposites[currentTheme]);
+		document.body.className = `${themeOpposites[themePreference]}-theme`;
+		themeToggleButton.textContent = `Schakel naar ${themeOpposites[themePreference] === "dark" ? "Licht" : "Donker"} thema`;
+		localStorage.setItem("preference", themeOpposites[themePreference]);
 	}
+
+	function loadTheme() {
+		const themePreference = localStorage.getItem("preference") || "dark";
+
+		document.body.className = `${themePreference}-theme`;
+		themeToggleButton.textContent = `Schakel naar ${themePreference === "dark" ? "Licht" : "Donker"} thema`;
+	}
+
+	loadTheme();
 });
